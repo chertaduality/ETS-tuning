72776258ff93f3bfe472befa2314b31bc318b0fd
chertaduality <cherta.duality@gmail.com>
2023-06-21 12:29:40 +0300

    parallax

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -10,14 +10,62 @@ import Footer from './../footer/Footer';
 import Copyright from '../footer/Copyright';
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
-
+import { useParallax, ParallaxProvider, ParallaxBanner } from 'react-scroll-parallax';
+import bg1 from "../../assets/images/parallax/flying-debris-png-3.png";
+import bg2 from "../../assets/images/parallax/dust-overlay-3.png";
+import bg3 from "../../assets/images/parallax/112-1123638_particles-png-picture-transparent-particle-effect-png.png";
+import bg4 from "../../assets/images/parallax/83208-dust-explosion-holi-particle-free-download-png-hd.png";
+import bg5 from "../../assets/images/parallax/dust-clipart-film-3.png";
 
 
 const Main = () => {
-
+  const { ref } = useParallax({ speed: 10 });
+    
+ 
+  
   return (
     <div>
-      <div className="w-full h-auto bg-bodyColor text-lightText">
+       <ParallaxProvider>
+      <div ref={ref} className="w-full h-auto bg-bodyColor text-lightText">
+      <ParallaxBanner
+            className="banner"
+            layers={[
+              {
+                image: bg1,
+                translateY: [0, 50],
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+              // {
+              //   image: bg2,
+              //   translateY: [5, 45],
+
+              //   shouldAlwaysCompleteAnimation: true,
+              //   expanded: false,
+              // },
+              {
+                image: bg3,
+                translateY: [10, 30],
+
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+              {
+                image: bg4,
+                translateY: [15, 25],
+
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+              {
+                image: bg5,
+                translateY: [20, 20],
+
+                shouldAlwaysCompleteAnimation: true,
+                expanded: false,
+              },
+            ]}
+          >
         <Banner />
         <div className="max-w-screen-xl mx-auto ">
         <AnimationOnScroll animateIn="animate__fadeIn">
@@ -38,7 +86,9 @@ const Main = () => {
           {/* <Footer /> */}
           {/* <Copyright /> */}
         </div>
+        </ParallaxBanner>
       </div>
+      </ParallaxProvider>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import 'slick-carousel/slick/slick.css';
 import { HashRouter } from 'react-router-dom';
+// import { ParallaxProvider } from 'react-scroll-parallax';
+import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ParallaxProvider>
     <HashRouter>
     <App />
     </HashRouter>
+    </ParallaxProvider>
   </React.StrictMode>
 );
