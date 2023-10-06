00ff538a6c89e69cee55bdff67af78416a5c0c65
chertaduality <cherta.duality@gmail.com>
2023-10-06 11:51:20 +0300

    mob menu fix

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -136,30 +136,103 @@ const Navbar = (_id, title, link) => {
           <FiMenu />
         </span>
         {showMenu && (
-          <div className="w-[100%] h-screen overflow-scroll absolute top-0 left-0 bg-mobMenuColor p-14 scrollbar-hide backdrop-blur">
+          <div className="w-[100%]  overflow-scroll absolute top-0 left-0 bg-mobMenuColor p-14 scrollbar-hide backdrop-blur">
             <div className="flex flex-col gap-8 py-2 relative">
               <div>
                 <img className="h-[80px] " src={logo} alt="logo" />
               </div>
               <ul className="flex flex-col gap-4">
-                {navLinksdata.map((item) => (
-                  <li
-                    key={item._id}
-                    className="text-xl font-semibold text-black tracking-wide cursor-pointer hover:text-designColor duration-300"
-                  >
-                    <Link
-                      onClick={() => setShowMenu(false)}
-                      activeClass="active"
-                      to={item.link}
-                      spy={true}
-                      smooth={true}
-                      offset={-70}
-                      duration={1000}
-                    >
-                      {item.title}
-                    </Link>
-                  </li>
-                ))}
+              <li
+            className="font-bodyFont font-semibold text-lg  text-black tracking-wide cursor-pointer hover:text-designColor duration-300 "
+            key={_id}
+            onClick={scrollToTop}
+          >
+            <Link
+              activeClassN="active"
+              to="/"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={2000}
+            >
+              Главная
+            </Link>
+          </li>
+          <li
+            className="font-bodyFont font-semibold text-lg whitespace-nowrap text-black tracking-wide cursor-pointer hover:text-designColor duration-300 "
+            key={_id}
+          >
+            <Link
+              activeClass="active"
+              to="/#about"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={2000}
+            >
+              О нас
+            </Link>
+          </li>
+          <li
+            className="font-bodyFont font-semibold text-lg  text-black tracking-wide cursor-pointer hover:text-designColor duration-300 "
+            key={_id}
+          >
+            <Link
+              activeClass="active"
+              to="/#skills"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={2000}
+            >
+              Услуги
+            </Link>
+          </li>
+          <li
+            className="font-bodyFont font-semibold text-lg  text-black tracking-wide cursor-pointer hover:text-designColor duration-300 "
+            key={_id}
+          >
+            <Link
+              activeClass="active"
+              to="/#projects"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={2000}
+            >
+              Проекты
+            </Link>
+          </li>
+          <li
+            className="font-bodyFont font-semibold text-lg  text-black tracking-wide cursor-pointer hover:text-designColor duration-300 "
+            key={_id}
+          >
+            <Link
+              activeClass="active"
+              to="/#partners"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={2000}
+            >
+              Партнеры
+            </Link>
+          </li>
+          <li
+            className="font-bodyFont font-semibold text-lg  text-black tracking-wide cursor-pointer hover:text-designColor duration-300 "
+            key={_id}
+          >
+            <Link
+              activeClass="active"
+              to="/#contacts"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={2000}
+            >
+              Контакты
+            </Link>
+          </li>
               </ul>{' '}
               <span
                 onClick={() => setShowMenu(false)}
